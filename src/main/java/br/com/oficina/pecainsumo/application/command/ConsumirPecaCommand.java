53ec7597a1ce63284a0d8df44bcf4978f48e85e6
Nícolas Gruger <nicolas.gruger.g@hotmail.com>
2026-04-15 11:53:58 -0300

    Revert "feat(pecas): classes faltantes consumir peça"
    This reverts commit f42774371d5e8ee761c180a981bddd64d96ea5fc.

--- src/main/java/br/com/oficina/pecainsumo/application/command/ConsumirPecaCommand.java
+++
@@ -1,6 +0,0 @@
-package br.com.oficina.pecainsumo.application.command;
-
-public record ConsumirPecaCommand(
-        String id,
-        int quantidade) {
-}
--- src/main/java/br/com/oficina/pecainsumo/application/usecase/ConsumirPecaUseCase.java
+++
@@ -1,7 +0,0 @@
-package br.com.oficina.pecainsumo.application.usecase;
-
-import br.com.oficina.pecainsumo.application.command.ConsumirPecaCommand;
-
-public interface ConsumirPecaUseCase {
-    void consumirPeca(ConsumirPecaCommand command);
-}
